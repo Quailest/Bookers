cdde2f44098d71ddb2eb73f4d10a5287c211cc31
EC2 Default User <ec2-user@ip-172-31-14-198.ec2.internal>
2022-01-16 18:15:04 +0000

    [Rest]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,23 @@
 class BooksController < ApplicationController
   def new
-    @book=Book.new
+    @book=book.new
   end
 
   def create
-    book=Book.new(book_params)
+    @book=Book.new(book_params)
 
-    book.save
+    if @book.save
+      redirect_to book_path(@book.id), notice:'Book was successfully created'
+    else
+      @books=Book.all
+      render :index
+    end
 
-    redirect_to '/top'
   end
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -25,20 +30,24 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice:'Book wa successfully updated'
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
-  book = Book.find(params[:id])
-  book.destroy
-  redirect_to '/books'
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
-
+  # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
